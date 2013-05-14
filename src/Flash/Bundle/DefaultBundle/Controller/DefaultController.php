200a4d196566cda652cddcfcd057c51287bd7fab
Ilia <yakov.the.smart@gmail.com>
2013-05-14 09:54:02 +0300

    Some point

--- src/Flash/Bundle/DefaultBundle/Controller/DefaultController.php
+++ src/Flash/Bundle/DefaultBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Flash\Bundle\DefaultBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller {
 
@@ -12,7 +13,7 @@ class DefaultController extends Controller {
      * @Route("/main", name="main_page")
      * @Template()
      */
-    public function mainAction($id = null) {
+    public function mainAction() {
 
         $user = $this->get('security.context')->getToken()->getUser();
 
@@ -20,6 +21,27 @@ class DefaultController extends Controller {
             'firstName' => $user->getFirstName(),
             'lastName' => $user->getLastName()
         );
+    }
+
+    /**
+     * @Route("/id{id}", name="user_page")
+     * @Template()
+     */
+    public function userAction($id = null) {
+
+        
+        $em = $this->getDoctrine()->getManager();
+        $acc = $em->getRepository('FlashDefaultBundle:Account')->find($id);
+            
+        if (null != $acc) {
+//            exit($id);
+            return array(
+                'firstName' => $acc->getFirstName(),
+                'lastName' => $acc->getLastName()
+            );
+        } else {
+            throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException('Page not found');
+        }
     }
 
 }
